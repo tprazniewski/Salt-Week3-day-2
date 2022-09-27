c65927422ed259c3566063079cf4b7e90d5efa5f
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-09-27 16:17:05 +0200

    create home route in the profile

--- ContactList/screens/CustomerProfile.js
+++ ContactList/screens/CustomerProfile.js
@@ -1,37 +1,39 @@
-import React from 'react'
-import {Text,StyleSheet, Image} from 'react-native'
-import {customers} from '../data/customers'
-import {View} from 'react-native'
-
+import React from "react";
+import { Text, StyleSheet, Image,Button } from "react-native";
+import { customers } from "../data/customers";
+import { View } from "react-native";
 
 // function CustomerProfile( {route, navigation}) {
-function CustomerProfile( prop) {
-    const customer = prop.route.params
-    console.log(customer)
-    return (
-
-    <View style={styles.container} >
-        <Image source={{uri: customer.picture.medium}} style={styles.logo}/>
+function CustomerProfile(prop) {
+  const customer = prop.route.params;
+  console.log(customer);
+  return (
+    <View style={styles.container}>
+      <View style={styles.title}>
+        <Text>Details Screen</Text>
+        <Button title="Go home" onPress={() => prop.navigation.navigate("List")} />
+      </View>
+      <View >
+        <Image source={{ uri: customer.picture.medium }} style={styles.logo} />
         <Text> {customer.name.first + " " + customer.name.last}</Text>
         <Text> {customer.email}</Text>
         <Text> {customer.phone}</Text>
+      </View>
     </View>
-  )
+  );
 }
 
-export default CustomerProfile
-
+export default CustomerProfile;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-
-    },
-    logo: {
-        width: 66,
-        height: 58,
-      }
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  },
+});
